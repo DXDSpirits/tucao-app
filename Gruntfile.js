dfa3e5422469c45c484d23d897f714377fcd85cc
Xindong Ding <dxd.spirits@gmail.com>
2013-12-24 00:14:26 +0800

    Resolve peerDependency issue (grunt-steroids needs grunt@0.4.1, exact version 0.4.1). Remove unuseful images.

--- Gruntfile.js
+++ Gruntfile.js
@@ -159,13 +159,10 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-includes');
-	grunt.loadNpmTasks('grunt-gettext');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-concurrent');
-	grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks("grunt-steroids");
 	
 	// Configurable port number
